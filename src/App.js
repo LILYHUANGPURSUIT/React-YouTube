452be6e5fc11053ff44c99e6de264c53e3d2dd31
“Lili <lilyhuang114classb@gmail.com>
2021-12-07 12:46:55 -0500

    Created Navbar.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
+import React from 'react';
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import HomePage from "./components/HomePage";
+import AboutPage from "./components/AboutPage";
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(){
+    super();
+
+  }
+
+
+  render(){
+    return (
+      <div className="App">
+        
+        <Router>
+          <Navbar />
+          
+            <Switch>
+              <Route exact path="/">
+                <HomePage />
+              </Route>
+              <Route path="/about">
+                <AboutPage />
+              </Route>
+            </Switch>
+          
+        </Router>
+
+        {/* <SearchBar />
+        <VideosDisplay />
+        <Comments /> */}
+        
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/components/AboutPage.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+class AboutPage extends React.Component{
+
+    render(){
+        return (
+            <div>
+                About Page
+            </div>
+        )
+    }
+}
+
+export default AboutPage;
