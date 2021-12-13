deb1e58f630e5215aadeed8edac091761fdcf45c
Larisa <larisashapkinab@gmail.com>
2021-12-13 16:18:31 -0500

    View Page change

--- src/components/ViewPage.js
+++ src/components/ViewPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {Link} from "react-router-dom";
+// import {Link} from "react-router-dom";
 import YouTube from "react-youtube";
 import Comments from "./Comments"
 
